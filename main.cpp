fed8474439c0de371beb39cdb6e3053b9b7b471e
Bodzioo44 <Bodzioo44@proton.me>
2024-07-17 20:02:27 +0200

    removed useless includes from main

--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
 #include "MainWindow.h"
 #include <QtWidgets/QApplication>
 
-#include "utils.h"
-#include "json11/json11.hpp"
-
-#include <iostream>
-#include <string>
-#include <map>
-using namespace std;
-using namespace json11;
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
