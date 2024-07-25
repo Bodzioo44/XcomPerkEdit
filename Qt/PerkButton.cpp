ac6367b2c1f80c8ed1ae7470b3d221e859297514
Bodzioo44 <Bodzioo44@proton.me>
2024-07-25 15:42:45 +0200

    Moved loading perk data to PerkButton

--- Qt/PerkButton.cpp
+++ Qt/PerkButton.cpp
@@ -1,5 +1,45 @@
 #include "PerkButton.h"
 
+PerkDisplayMap load_perk_display(std::vector<Perk> perks) {
+    std::sort(perks.begin(), perks.end(), [](const Perk& a, const Perk& b) { return a.index < b.index; });
+    std::ifstream file("../assets/All_Perk_Data.txt");
+    if (!file.is_open()) {
+        throw std::runtime_error("Could not open file: ../assets/All_Perk_Data.txt");
+    }
+    std::string line;
+    int current_line = 1;
+    unsigned int current_perk = 0;
+    bool correct_perk = false;
+    PerkDisplay current_PerkDisplay;
+    PerkDisplayMap perk_display_map;
+    while (std::getline(file, line)) {
+        if (current_line % 4 == 1 && std::stoi(line) == perks[current_perk].index) {
+            correct_perk = true;
+            current_PerkDisplay.extra_stats = "(Mobility:" + QString::number(perks[current_perk].stats.mobility) + ", Aim:" + QString::number(perks[current_perk].stats.aim) + ", Will:" + QString::number(perks[current_perk].stats.will) + ")";
+        }
+        else if (correct_perk) {
+            if (current_line % 4 == 2) {
+                current_PerkDisplay.name = QString::fromStdString(line);
+            }
+            else if (current_line % 4 == 3) {
+                current_PerkDisplay.description = QString::fromStdString(line);
+            }
+            else if (current_line % 4 == 0) {
+                current_PerkDisplay.icon_path = QString::fromStdString("../assets/icons/" + line);
+                perk_display_map[perks[current_perk].index] = current_PerkDisplay;
+                correct_perk = false;
+                current_perk++;
+                if (current_perk == perks.size()) {
+                    break;
+                }
+            }
+        }
+        current_line++;
+    }
+    return perk_display_map;
+}
+
+
 PerkButton::PerkButton(QWidget* parent) : QToolButton(parent) {
     //TODO: try to move this inside .ui file
     size = 60;
@@ -8,16 +48,9 @@ PerkButton::PerkButton(QWidget* parent) : QToolButton(parent) {
     this->setStyleSheet(StyleSheet);
 }
 
-//TODO: change Perk to soldier_stats
-void PerkButton::LoadPerk(const PerkAssets icon_assets, Perk current_perk) {
-    QString icon_path =  QString::fromStdString("../assets/icons/" + icon_assets.icon_path);
-    QString description = icon_assets.description.c_str();
-    QString name = icon_assets.name.c_str();
-    QString extra_stats = "(Mobility:" + QString::number(current_perk.stats.mobility) + ", Aim:" + QString::number(current_perk.stats.aim) + ", Will:" + QString::number(current_perk.stats.will) + ")";
-
-    setToolTip("<b>" + name + "</b><br>" + description + "<br>" + extra_stats);
-
-    QIcon unscaled_icon(icon_path);
+void PerkButton::LoadPerk(const PerkDisplay& perk_display) {
+    setToolTip("<b>" + perk_display.name + "</b><br>" + perk_display.description + "<br>" + perk_display.extra_stats);
+    QIcon unscaled_icon(perk_display.icon_path);
     QPixmap pixmap = unscaled_icon.pixmap(size, size).scaled(size, size, Qt::KeepAspectRatio, Qt::SmoothTransformation);
     icon = pixmap;
 
