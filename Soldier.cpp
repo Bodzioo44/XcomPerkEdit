dfa0be0e210e360387ee7aa756e08ea29017fadd
Bodzioo44 <Bodzioo44@proton.me>
2024-07-26 19:11:34 +0200

    Added soldier class

---
+++ Soldier.cpp
@@ -0,0 +1,127 @@
+#include "Soldier.h"
+
+//PerkSet GetSoldiers::perks(const xcom::property_list* properties_ptr);
+
+Soldier::Soldier(xcom::checkpoint& soldier): properties(soldier.properties) {
+    starting_stats = GetSoldiers::stats(properties);
+    difference_stats = SoldierStats();
+    perks = GetSoldiers::perks(properties);
+
+};
+
+void Soldier::EnablePerk(int index) {
+    Perk& perk = perks[index];
+    if (!perk.enabled) {
+        perk.enabled = true;
+        perk.value++;
+        difference_stats += perk.stats;
+    }
+}
+
+void Soldier::DisablePerk(int index) {
+    Perk& perk = perks[index];
+    if (perk.enabled) {
+        perk.enabled = false;
+        perk.value--;
+        difference_stats -= perk.stats;
+    }
+}
+
+xcom::property_list& Soldier::GetPropertyList() const{
+    return properties;
+}
+
+LabelSet Soldier::GetLabels() const{
+        std::string mobility_label = "<b>Mobility: "+ std::to_string(starting_stats.mobility) + "</b>";
+        std::string aim_label = "<b>Aim: " + std::to_string(starting_stats.aim) + "</b>";
+        std::string will_label = "<b>Will: " + std::to_string(starting_stats.will) + "</b>";
+
+        if (difference_stats.mobility > 0) {
+            mobility_label += "<b><font color='green'> +" + std::to_string(difference_stats.mobility) + "</font></b>";
+        }
+        else if (difference_stats.mobility < 0) {
+            mobility_label += "<b><font color='red'> " + std::to_string(difference_stats.mobility) + "</font></b>";
+        }
+        if (difference_stats.aim > 0) {
+            aim_label += "<b><font color='green'> +" + std::to_string(difference_stats.aim) + "</font></b>";
+        }
+        else if (difference_stats.aim < 0) {
+            aim_label += "<b><font color='red'> " + std::to_string(difference_stats.aim) + "</font></b>";
+        }
+        if (difference_stats.will > 0) {
+            will_label += "<b><font color='green'> +" + std::to_string(difference_stats.will) + "</font></b>";
+        }
+        else if (difference_stats.will < 0) {
+            will_label += "<b><font color='red'> " + std::to_string(difference_stats.will) + "</font></b>";
+        }
+        return {mobility_label, aim_label, will_label};   
+}
+
+PerkSet Soldier::GetPerks() const {
+    return perks;
+}
+
+void Soldier::UpdateSoldier() {
+    
+}
+
+namespace GetSoldiers {
+    std::string class_type(const xcom::property_list& properties) {
+        xcom::struct_property& m_kSoldier = static_cast<xcom::struct_property&> (*properties[1]);
+        xcom::struct_property& kClass = static_cast<xcom::struct_property&> (*m_kSoldier.properties[11]);
+        return static_cast<xcom::string_property&> (*kClass.properties[0]).str.str;
+    }
+    std::string full_name(const xcom::property_list& properties) {
+        xcom::struct_property& m_kSoldier = static_cast<xcom::struct_property&> (*properties[1]);
+        xcom::string_property& strFirstName = static_cast<xcom::string_property&> (*m_kSoldier.properties[1]);
+        xcom::string_property& strLastName = static_cast<xcom::string_property&> (*m_kSoldier.properties[2]);
+        xcom::string_property& strNickName = static_cast<xcom::string_property&> (*m_kSoldier.properties[3]);
+        return strFirstName.str.str + '"' + strNickName.str.str + '"' + strLastName.str.str;
+    }
+    //TODO: check if this works?
+    std::string eStatus(const xcom::property_list& properties) {
+        xcom::enum_property& m_eStatus = static_cast<xcom::enum_property&> (*properties[3]);
+        return m_eStatus.value.name;
+    }
+
+    int rank(const xcom::property_list& properties) {
+        xcom::struct_property& m_kSoldier = static_cast<xcom::struct_property&> (*properties[1]);
+        return static_cast<xcom::int_property&> (*m_kSoldier.properties[4]).value;
+    }
+
+    PerkSet perks(const xcom::property_list& properties) {
+        xcom::struct_property& m_kChar = static_cast<xcom::struct_property&> (*properties[0]);
+        xcom::static_array_property& aUpgrades = static_cast<xcom::static_array_property&> (*m_kChar.properties[3]);
+
+        std::string path = "../assets/" + class_type(properties) + ".txt";
+        std::ifstream file(path);
+        if (!file.is_open()) {
+            throw std::runtime_error("Could not open file: " + path);
+        }
+
+        PerkSet perks;
+        std::string line;
+        int i = 0;
+        //loads perks from the file in format: index, mobility, aim, will
+        //same perk can give different stats for different classes.
+        while(std::getline(file, line)) {
+            std::vector<std::string> row;
+            std::stringstream ss(line);
+            std::string value;
+            while (std::getline(ss, value, ',')) {
+                row.push_back(value);
+            }
+            //pulls index value from the save file
+            int index_value = static_cast<xcom::int_property*> (aUpgrades.properties[std::stoi(row[0])].get())->value;
+            perks[i] = Perk(std::stoi(row[0]), index_value, SoldierStats(std::stoi(row[1]), std::stoi(row[2]), std::stoi(row[3])));
+            i++;
+        }
+        return perks;
+    }
+
+    SoldierStats stats(const xcom::property_list& properties) {
+        xcom::struct_property& m_kChar = static_cast<xcom::struct_property&> (*properties[0]);
+        xcom::static_array_property& aStats = static_cast<xcom::static_array_property&> (*m_kChar.properties[6]);
+        return SoldierStats(static_cast<xcom::int_property&> (*aStats.properties[3]).value, static_cast<xcom::int_property&> (*aStats.properties[1]).value, static_cast<xcom::int_property&> (*aStats.properties[7]).value);
+    }
+}
---
+++ Soldier.h
@@ -0,0 +1,79 @@
+#ifndef SOLDIER_H
+#define SOLDIER_H
+
+#include "xcomsave/xcom.h"
+#include <fstream>
+#include <sstream>
+
+struct SoldierStats {
+    int mobility;
+    int aim;
+    int will;
+
+    SoldierStats(int mobility = 0, int aim = 0, int will = 0): mobility(mobility), aim(aim), will(will) {}
+
+    void operator += (const SoldierStats& stats) {
+        mobility = mobility + stats.mobility;
+        aim = aim + stats.aim;
+        will = will + stats.will;
+    }
+
+    void operator -= (const SoldierStats& stats) {
+        mobility = mobility - stats.mobility;
+        aim = aim - stats.aim;
+        will = will - stats.will;
+    }
+};
+
+
+struct Perk {
+    int index;
+    int value;
+    SoldierStats stats;
+    bool enabled;
+    //TODO: confirm this.
+    //sometimes perks connected to items have values greater than 1. (f.e. Smoke Grenade, Battle Scanner, Shredder Ammo)
+    //from my testing, value represents number of items in the inventory*2 (+ 1 if the perk is enabled)
+    //so for example value of 5 means 2 items in the inventory, and the perk is enabled.
+    //and value of 2 means 1 item in the inventory, and the perk is disabled.
+    Perk(int index = 0, int value = 0, SoldierStats stats = {0, 0, 0}): index(index), value(value), stats(stats) {
+        if (value % 2 == 1) {
+            enabled = true;
+        }
+        else {
+            enabled = false;
+        }
+    }
+};
+
+using PerkSet = std::array<Perk, 18>;
+using LabelSet = std::array<std::string, 3>;
+
+class Soldier {
+    public:
+        Soldier(xcom::checkpoint& soldier);
+        void EnablePerk(int index);
+        void DisablePerk(int index);
+        xcom::property_list& GetPropertyList() const;
+        LabelSet GetLabels() const;
+        PerkSet GetPerks() const;
+        void UpdateSoldier();
+
+    private:
+        xcom::property_list& properties;
+        PerkSet perks;
+        SoldierStats starting_stats;
+        SoldierStats difference_stats;
+};
+
+//look into property_visitor again? is there a better way to fo it?
+namespace GetSoldiers {
+    std::string class_type(const xcom::property_list& properties);
+    std::string full_name(const xcom::property_list& properties);
+    std::string eStatus(const xcom::property_list& properties);
+    int rank(const xcom::property_list& properties);
+    PerkSet perks(const xcom::property_list& properties);
+    SoldierStats stats(const xcom::property_list& properties);
+}
+
+#endif // SOLDIER_H
