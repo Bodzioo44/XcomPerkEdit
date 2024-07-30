b843ec1cff2cfa9fd5b4258abed7044a527c0c99
Bodzioo44 <160941088+Bodzioo44@users.noreply.github.com>
2024-07-30 13:14:19 +0100

    Added app icon

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "MainWindow.h"
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtGui/QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/assets/icons/app-icon.png"));
     MainWindow w;
     w.show();
     return a.exec();
